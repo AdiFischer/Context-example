faa042038cb7bffc8a524f402045b3ddc0c57f96
AdiFischer <adifischer1@gmail.com>
2022-11-03 11:08:15 -0400

    ready to use context

---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,7 @@
+export default function MenuBar() {
+    return (
+        <nav>
+            <p>MenuBar</p>
+        </nav>
+    )
+}
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,8 @@
+export default function welcome() {
+    return (
+        <section>
+            <h2>welcome</h2>
+    
+        </section>
+    )
+}
